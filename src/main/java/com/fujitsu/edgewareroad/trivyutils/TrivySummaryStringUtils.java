c3bd8db4b34d4d7bdb6ccc3937479f588440a250
Phil Pinel <35564100+EdgewareRoad@users.noreply.github.com>
2024-03-27 16:00:18 +0000

    More intelligent word wrapping, looking for CamelCase and known version number or tag separators

--- src/main/java/com/fujitsu/edgewareroad/trivyutils/TrivySummaryStringUtils.java
+++ src/main/java/com/fujitsu/edgewareroad/trivyutils/TrivySummaryStringUtils.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 public class TrivySummaryStringUtils {
     private final Pattern whitespaceSplitter = Pattern.compile("\s+");
+    private final Pattern longWordSplitter = Pattern.compile("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])|(?<!^)(?=[:\\-/.])");
     private final int WORD_LEN_BEFORE_HYPHENATE = 18;
 
     public String getHyphenated(String input)
@@ -23,11 +24,25 @@ public class TrivySummaryStringUtils {
             else
             {
                 // We need to be able to break this word anywhere after the max word length per line
-                builder.append(word.substring(0, WORD_LEN_BEFORE_HYPHENATE));
-                word.substring(WORD_LEN_BEFORE_HYPHENATE).chars().forEach(c -> {
-                    builder.append((char)0x00AD);
-                    builder.append((char)c);
-                });
+                boolean firstInWord = true;
+                for (String subWord : longWordSplitter.split(word))
+                {
+                    if (!firstInWord) builder.append((char)0x00AD);    // Silent hyphen
+                    firstInWord = false;
+                    if (subWord.length() < WORD_LEN_BEFORE_HYPHENATE)
+                    {
+                        builder.append(subWord);
+                    }
+                    else
+                    {
+                        // Well we have no choice but to split the subword in an ugly way.
+                        builder.append(subWord.substring(0, WORD_LEN_BEFORE_HYPHENATE - 1));
+                        subWord.substring(WORD_LEN_BEFORE_HYPHENATE - 1).chars().forEach(c -> {
+                            builder.append((char)0x00AD);
+                            builder.append((char)c);
+                        });
+                    }
+                }
             }
         }
 
